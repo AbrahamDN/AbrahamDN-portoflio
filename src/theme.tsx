2ce969b18896f9ae5c641494af43232414a1fd2b
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-06 18:43:50 +0100

    feat: Theme  - primary variant

--- src/theme.tsx
+++ src/theme.tsx
@@ -19,8 +19,41 @@ const theme = extendTheme({
           borderImage:
             "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)",
           borderWidth: "2px",
+          borderStyle: "solid",
           borderImageSlice: 1,
           borderTop: "none",
+        },
+      },
+    },
+    Button: {
+      baseStyle: {
+        borderRadius: "full",
+        pt: "6",
+        pb: "7",
+      },
+      variants: {
+        primary: {
+          fontSize: "xl",
+          border: "1px solid black",
+          px: "10",
+        },
+      },
+    },
+    Link: {
+      variants: {
+        primary: {
+          fontWeight: "semibold",
+          border: "1px solid black",
+          borderRadius: "full",
+          px: "10",
+          py: "2",
+          pb: "3",
+          _hover: {
+            textDecor: "none",
+          },
+          _focus: {
+            textDecor: "none",
+          },
         },
       },
     },
