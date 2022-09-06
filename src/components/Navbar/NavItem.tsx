e0a46ae78b15c9e8b974f61a9b4e03433ec53a33
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-06 18:08:37 +0100

    feat: NavItem

---
+++ src/components/Navbar/NavItem.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Stack, Text, Link, Divider, Center } from "@chakra-ui/react";
+
+type NavItemProps = {
+  onClick: (val: any) => any;
+  href: string;
+  text: string;
+  noDivider?: boolean;
+};
+
+function NavItem({ onClick, noDivider, href, text }: NavItemProps) {
+  return (
+    <Stack as="li" className="nav-item" minW="40">
+      <Link
+        href={href}
+        py="4"
+        _hover={{ textDecor: "none" }}
+        _focus={{ textDecor: "none" }}
+        onClick={onClick}
+      >
+        <Text as="span" fontSize="2xl" fontWeight="semibold">
+          {text}
+        </Text>
+      </Link>
+
+      {!noDivider && (
+        <Center>
+          <Divider
+            orientation="horizontal"
+            borderColor="black"
+            w="20"
+            transition="width 300ms ease-in-out"
+            variant="gradient"
+          />
+        </Center>
+      )}
+    </Stack>
+  );
+}
+
+export default NavItem;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,40 +1,54 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+  sm: "40em",
+  md: "52em",
+  lg: "64em",
+  xl: "80em",
+});
 
 const theme = extendTheme({
+  components: {
+    Divider: {
+      variants: {
+        gradient: {
+          border: "1px solid",
+          borderImage:
+            "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%)",
+          borderWidth: "2px",
+          borderImageSlice: 1,
+          borderTop: "none",
+        },
+      },
+    },
+  },
   semanticTokens: {
     colors: {
       text: {
-        default: '#16161D',
-        _dark: '#ade3b8',
+        default: "#16161D",
+        _dark: "#ade3b8",
       },
       heroGradientStart: {
-        default: '#7928CA',
-        _dark: '#e3a7f9',
+        default: "#7928CA",
+        _dark: "#e3a7f9",
       },
       heroGradientEnd: {
-        default: '#FF0080',
-        _dark: '#fbec8f',
+        default: "#FF0080",
+        _dark: "#fbec8f",
       },
     },
     radii: {
-      button: '12px',
+      button: "12px",
     },
   },
   colors: {
-    black: '#16161D',
+    black: "#16161D",
   },
   fonts,
   breakpoints,
-})
+});
 
-export default theme
+export default theme;
