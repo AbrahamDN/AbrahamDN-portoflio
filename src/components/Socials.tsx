776472037148da29f204e208394265592bdcc7ff
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-08 19:33:12 +0100

    feat: Socials component

---
+++ src/components/Socials.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Flex, FlexProps, Link, Tooltip } from "@chakra-ui/react";
+import { GithubIcon, LinkedInIcon } from "./Icons";
+
+const Socials = (props: FlexProps) => {
+  return (
+    <Flex gap="4" {...props}>
+      <Tooltip label="Github" color="white">
+        <Link href="https://github.com/AbrahamDN" isExternal>
+          <GithubIcon w="6" h="6" />
+        </Link>
+      </Tooltip>
+
+      <Tooltip label="LinkedIn" color="white">
+        <Link href="https://www.linkedin.com/in/abrahamdn/" isExternal>
+          <LinkedInIcon w="6" h="6" />
+        </Link>
+      </Tooltip>
+    </Flex>
+  );
+};
+
+export default Socials;
