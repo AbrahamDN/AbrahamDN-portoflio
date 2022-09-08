0c98eb7ccba64847d1406bfb3206f52db5c1fdda
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-08 23:32:43 +0100

    feat: data

---
+++ data/headerLinks.tsx
@@ -0,0 +1,6 @@
+export const headerLinks = [
+  { title: "home", path: "/" },
+  { title: "skills", path: "#skills" },
+  { title: "projects", path: "#projects" },
+  // { title: "blog", path: "/" },
+];
---
+++ data/projects.tsx
@@ -0,0 +1,42 @@
+export const featuredCards = [
+  {
+    mobileImage:
+      "https://res.cloudinary.com/abrahamdn/image/upload/v1661209824/abrahamdn/telimine-mobile_xcfyic.png",
+    image:
+      "https://res.cloudinary.com/abrahamdn/image/upload/v1661210713/abrahamdn/telimine-desktop_vljdp9.png",
+    tags: [{ title: "React" }, { title: "Stripe" }, { title: "SCSS" }],
+    title: "TeliMine",
+    description:
+      "Built with React.js, TeliMine is a online phone store selling modern mobile phones from the leading brands.",
+    previewLink: "https://telimine.netlify.app/",
+    repoLink: "https://github.com/AbrahamDN/react-phone-store-telimine",
+  },
+  {
+    mobileImage:
+      "https://res.cloudinary.com/abrahamdn/image/upload/v1661213266/abrahamdn/pro-splash-mobile_ed9zdq.png",
+    image:
+      "https://res.cloudinary.com/abrahamdn/image/upload/v1661215325/abrahamdn/pro-splash-desktop_qseldc.png",
+    tags: [{ title: "GSAP" }, { title: "Javascript" }, { title: "CSS" }],
+    title: "Pro Splash",
+    description:
+      "Pro-Splash is a collaborative portfolio blog/site that introduces 3 professionals, each in different industries.",
+    previewLink: "https://abrahamdn.github.io/Pro-Splash/",
+    repoLink: "https://github.com/AbrahamDN/Pro-Splash",
+  },
+  {
+    mobileImage:
+      "https://res.cloudinary.com/abrahamdn/image/upload/v1661213266/abrahamdn/crwn-clothing-mobile_z4o0ao.png",
+    image:
+      "https://res.cloudinary.com/abrahamdn/image/upload/v1661213266/abrahamdn/crwn-clothing-desktop_vxexoi.png",
+    tags: [
+      { title: "React" },
+      { title: "Redux" },
+      { title: "Firebase" },
+      { title: "Styled components" },
+    ],
+    title: "Crwn Clothing",
+    description: "Shopping and checkout with Stripe.",
+    previewLink: "https://crwn-clothingltb.herokuapp.com/",
+    repoLink: "https://github.com/AbrahamDN/crown-clothing",
+  },
+];
