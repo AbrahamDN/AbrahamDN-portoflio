8b3604c7fcdf75bc712e0f1820155b13c8187bf7
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-10 23:59:02 +0100

    refactor: SectionHeading - use children prop

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -1,11 +1,11 @@
 import { Divider, Heading, HeadingProps, Text } from "@chakra-ui/react";
 import React from "react";
 
-const SectionHeading = (props: HeadingProps & { title: string }) => {
+const SectionHeading = (props: HeadingProps) => {
   return (
     <Heading display="flex" alignItems="center" gap="8" {...props}>
       <Text as="span" fontSize={{ base: "2xl", md: "4xl" }}>
-        {props.title}
+        {props.children}
       </Text>
       <Divider flex={[1, "initial"]} variant="gradient" w="30%" />
     </Heading>
