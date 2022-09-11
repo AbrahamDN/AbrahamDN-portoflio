133312eca5a0fe3aeccd3380b0b581ce236c947e
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-09-11 02:12:30 +0100

    refactor: Hero section as component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,85 @@
-import { Flex, Heading } from '@chakra-ui/react'
+import {
+  AspectRatio,
+  Box,
+  Divider,
+  Flex,
+  Link,
+  Stack,
+  StackProps,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import { DownloadIcon } from "./Icons";
+import Section from "./Section";
+import Socials from "./Socials";
 
-export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
-)
+export const Hero = (props: StackProps) => {
+  const isMobile = useBreakpointValue({ base: true, sm: false });
+
+  return (
+    <Section
+      id="hero"
+      sx={{ ".chakra-container": { pr: { base: "6", sm: 0 } } }}
+      {...props}
+    >
+      <Flex minH="full" alignItems="center" gap="6">
+        <Stack
+          // flex="2"
+          maxW={{ base: "full", sm: "40ch", xl: "prose" }}
+          gap="4"
+        >
+          <Box as="h1" textStyle="h1" textTransform="uppercase">
+            Abraham D Ndinga
+          </Box>
+
+          <Text maxW="prose">
+            Experienced front-end developer with about 3 years of experience,
+            passionate about accessibility, performance and user experience. My
+            key skills consist of JavaScript, React, Styled Components,
+            Storybook & Redux all whilst utilising GitLab's CI/CD pipelines.
+          </Text>
+
+          <Link
+            variant="primary"
+            href="/assets/abrahamdn-cv.pdf"
+            target="_blank"
+            rel="noopener"
+          >
+            <Text as="span">Download CV</Text>
+            <DownloadIcon w="5" h="5" ml="0.5" />
+          </Link>
+
+          {isMobile && (
+            <Divider w="1" h="68px" orientation="vertical" variant="gradient" />
+          )}
+
+          <Socials />
+        </Stack>
+
+        {!isMobile && (
+          <AspectRatio
+            flex="1"
+            w="full"
+            ratio={1}
+            display="flex"
+            alignItems="center"
+          >
+            <Image
+              className="w-full h-full object-cover"
+              src="/assets/adn-avatar.svg"
+              alt="abrahamdn portrait"
+              width={533}
+              height={442}
+              // layout="responsive"
+            />
+          </AspectRatio>
+        )}
+      </Flex>
+    </Section>
+  );
+};
 
 Hero.defaultProps = {
-  title: 'with-chakra-ui-typescript',
-}
+  title: "with-chakra-ui-typescript",
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,19 @@
-import { DownloadIcon } from "../components/Icons";
 import {
-  AspectRatio,
   Box,
   Button,
   Center,
   Divider,
   Flex,
   Grid,
-  Heading,
-  Link,
   Stack,
   Tag,
   Text,
   Tooltip,
-  useBreakpointValue,
   useClipboard,
 } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import { Hero } from "../components/Hero";
 import Section from "../components/Section";
-import Socials from "../components/Socials";
-import Image from "next/image";
 import Card from "../components/Card";
 import { featuredCards } from "../../data/projects";
 import SectionHeading from "../components/SectionHeading";
@@ -43,7 +37,6 @@ const scrollbarStyles = {
 };
 
 const Index = () => {
-  const isMobile = useBreakpointValue({ base: true, sm: false });
   const { hasCopied, onCopy } = useClipboard("mail@abrahamdn.com");
 
   return (
@@ -76,69 +69,7 @@ const Index = () => {
         transform="translateY(-50%)"
       />
 
-      <Section
-        id="hero"
-        sx={{ ".chakra-container": { pr: { base: "6", sm: 0 } } }}
-      >
-        <Flex minH="full" alignItems="center" gap="6">
-          <Stack
-            // flex="2"
-            maxW={{ base: "full", sm: "40ch", xl: "prose" }}
-            gap="4"
-          >
-            <Box as="h1" textStyle="h1" textTransform="uppercase">
-              Abraham D Ndinga
-            </Box>
-
-            <Text maxW="prose">
-              Experienced front-end developer with about 3 years of experience,
-              passionate about accessibility, performance and user experience.
-              My key skills consist of JavaScript, React, Styled Components,
-              Storybook & Redux all whilst utilising GitLab's CI/CD pipelines.
-            </Text>
-
-            <Link
-              variant="primary"
-              href="/assets/abrahamdn-cv.pdf"
-              target="_blank"
-              rel="noopener"
-            >
-              <Text as="span">Download CV</Text>
-              <DownloadIcon w="5" h="5" ml="0.5" />
-            </Link>
-
-            {isMobile && (
-              <Divider
-                w="1"
-                h="68px"
-                orientation="vertical"
-                variant="gradient"
-              />
-            )}
-
-            <Socials />
-          </Stack>
-
-          {!isMobile && (
-            <AspectRatio
-              flex="1"
-              w="full"
-              ratio={1}
-              display="flex"
-              alignItems="center"
-            >
-              <Image
-                className="w-full h-full object-cover"
-                src="/assets/adn-avatar.svg"
-                alt="abrahamdn portrait"
-                width={533}
-                height={442}
-                // layout="responsive"
-              />
-            </AspectRatio>
-          )}
-        </Flex>
-      </Section>
+      <Hero />
 
       <Center>
         <Divider variant="gradient" maxW="container.xl" />
